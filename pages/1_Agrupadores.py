777196f2d793ffbf7c7bdad75ef9dc79cb3bc8f5
descudero <descudero@naturgy.com>
2025-04-24 07:53:06 +0200

    Add subheaders for file upload sections to improve UI clarity

--- pages/1_Agrupadores.py
+++ pages/1_Agrupadores.py
@@ -29,8 +29,10 @@ def main():
     col1, col2 = st.columns(2)
     # Subir archivos
     with col1:
+        st.subheader("PERSONAL")
         personal_file = st.file_uploader("Subir archivo Excel de PERSONAL", type=["xlsx", "xls"])
     with col2:
+        st.subheader("AGRPADORES")
         agrupadores_file = st.file_uploader("Subir archivo Excel de AGRUPADORES", type=["xlsx", "xls"])
 
     if personal_file and agrupadores_file:
